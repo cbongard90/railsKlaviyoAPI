c399823bfd55aa4f25df45e746dad8c45e39732b
Christian Bongard <christian.bongard90@gmail.com>
2022-10-11 01:35:50 +0400

    adding validation and gitignore for env file

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,10 @@
-class UsersController < ApplicationController
+require 'uri'
+require 'net/http'
+require 'openssl'
+
+URL = URI("https://a.klaviyo.com/api/v2/list/WjfAbK/members?api_key=#{ENV['KLAVIYO_API_KEY']}")
+
+class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: %i[ show update destroy ]
 
   # GET /users
@@ -18,7 +24,19 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: @user, status: :created
+
+      # Add user to Klaviyo list
+      http = Net::HTTP.new(URL.host, URL.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+      request = Net::HTTP::Post.new(URL)
+      request["Content-Type"] = "application/json"
+      request.body = "{\n\"profiles\": [\n{\n\"email\": \"#{@user.email}\",\n            \"phone_number\": \"#{@user.phone_number}\",\n            \"first_name\": \"#{@user.first_name}\",\n            \"last_name\": \"#{@user.last_name}\",\n            \"date_of_birth\": \"#{@user.date_of_birth}\", \"sms_consent\": true\n        }\n    ]\n}"
+
+      response = http.request(request)
+      puts response.read_body
     else
       render json: @user.errors, status: :unprocessable_entity
     end
