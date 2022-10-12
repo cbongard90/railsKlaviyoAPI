f16e6c81c8478d1afb22fc0f0bc6fdb83a37da2c
Christian Bongard <christian.bongard90@gmail.com>
2022-10-12 22:02:01 +0400

    removed the unecessary routes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,19 +5,19 @@ require 'openssl'
 URL = URI("https://a.klaviyo.com/api/v2/list/WjfAbK/members?api_key=#{ENV['KLAVIYO_API_KEY']}")
 
 class Api::V1::UsersController < ApplicationController
-  before_action :set_user, only: %i[ show update destroy ]
+  # before_action :set_user, only: %i[ show update destroy ]
 
   # GET /users
-  def index
-    @users = User.all
+  # def index
+  #   @users = User.all
 
-    render json: @users
-  end
+  #   render json: @users
+  # end
 
   # GET /users/1
-  def show
-    render json: @user
-  end
+  # def show
+  #   render json: @user
+  # end
 
   # POST /users
   def create
@@ -25,38 +25,34 @@ class Api::V1::UsersController < ApplicationController
 
     if @user.save
       # render json: @user, status: :ok
-
       klaviyo_response = post_to_klaviyo(@user)
-
       puts klaviyo_response
-
       render json: klaviyo_response, status: :ok
-
     else
       render json: @user.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      render json: @user
-    else
-      render json: @user.errors, status: :unprocessable_entity
-    end
-  end
+  # def update
+  #   if @user.update(user_params)
+  #     render json: @user
+  #   else
+  #     render json: @user.errors, status: :unprocessable_entity
+  #   end
+  # end
 
   # DELETE /users/1
-  def destroy
-    @user.destroy
-  end
+  # def destroy
+  #   @user.destroy
+  # end
 
   private
 
   # Use callbacks to share common setup or constraints between actions.
-  def set_user
-    @user = User.find(params[:id])
-  end
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
 
   # Only allow a list of trusted parameters through.
   def user_params
